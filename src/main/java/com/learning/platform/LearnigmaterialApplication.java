fe49a0ff8a61f7810f2d85d9e8e9382ff2cba87f
tushar <tusharkokane366@gmail.com>
2024-11-17 12:15:50 +0530

    changes made by mayur

--- src/main/java/com/learning/platform/LearnigmaterialApplication.java
+++ src/main/java/com/learning/platform/LearnigmaterialApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LearnigmaterialApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LearnigmaterialApplication.class, args);
+		 SpringApplication.run(LearnigmaterialApplication.class, args);
 	}
 
 }
--- src/main/java/com/learning/platform/enums/Role.java
+++ src/main/java/com/learning/platform/enums/Role.java
@@ -4,6 +4,6 @@ public enum Role {
 	
 	Student,
 	Instructor,
-	Admin
+	     Admin
 
 }
